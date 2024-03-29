bd934569ca66ef60ca3b04c6fc9fbde7dd996347
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2024-03-29 06:03:52 -0700

    Update Article.cs

--- 2288/Models/Article.cs
+++ 2288/Models/Article.cs
@@ -1,1 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        // Връзка с таблицата Admins (ако е нужен потребител, който е създал статията)
+        [ForeignKey("AuthorId")]
+        public int? AuthorId { get; set; } // Позволява null стойност
+        public Admin Author { get; set; }
+
+        // ... (Допълнителни свойства)
+    }
+}
 

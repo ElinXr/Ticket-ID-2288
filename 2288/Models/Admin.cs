1c6e85677c42d50dfffa06ac2e4f2a11d4478436
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2024-03-29 05:59:06 -0700

    Update Admin.cs

--- 2288/Models/Admin.cs
+++ 2288/Models/Admin.cs
@@ -1,1 +1,22 @@
 
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourProject.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        // ... (Допълнителни свойства)
+    }
+}
